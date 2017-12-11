a97e13c342a5dcbf702f740db4ae4f0c265b0a42
wesleydestroy <wesleydestroy@hotmail.com>
2017-12-11 04:42:44 -0200

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
-<?php 
+<?php
+
+session_start();
 
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \BrilhodeSol\Page;
 use \BrilhodeSol\PageAdmin;
+use \BrilhodeSol\Model\User;
 
 $app = new Slim();
 
@@ -18,11 +21,43 @@ $app->get('/', function() {
 
 });
 
-$app->get('/dono', function() {
+$app->get('/admin', function() {
+
+    User::verifyLogin();
 
     $page = new PageAdmin();
 
     $page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function (){
+
+    $page = new PageAdmin([
+        "header"=>false,
+        "footer"=>false
+
+    ]);
+
+    $page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function (){
+
+    User::login($_POST["login"], $_POST["password"] );
+
+    header("Location: /admin");
+    exit;
+});
+
+$app->get('/admin/logout', function (){
+
+    User::logout();
+
+    header("Location: /admin/login");
+
+    exit;
 
 });
 
