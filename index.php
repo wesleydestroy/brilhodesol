27b4c42377f7df08968985d0692bcc64fdda4fb9
wesleydestroy <wesleydestroy@hotmail.com>
2017-12-11 02:10:01 -0200

    criada a classe page

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \BrilhodeSol\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new BrilhodeSol\DB\Sql();
-	
-	$results = $sql->select("SELECT * FROM tb_users");
 
-	echo json_encode($results);
+    $page = new Page();
 
+    $page->setTpl("index");
 
 });
 
---
+++ views-cacheheader.8da865a32c986bf419ab813e0543d6e0.rtpl.php
@@ -0,0 +1,6 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Brilho de Sol</title>
+    </head>
